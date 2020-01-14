d49fc9388acaf33070d9ef36388e0e3694859e34
Turchick <56910528+Turchick@users.noreply.github.com>
2020-01-14 08:46:25 +0400

    Add files via upload

--- mnist_test.py
+++ mnist_test.py
@@ -1,3 +1,4 @@
+# import required modules
 import keras
 import sys
 import cv2
@@ -7,10 +8,9 @@ from keras.layers import Dense
 
 # импорт MNIST dataset
 from keras.datasets import mnist
-from PIL import ImageTk, Image
-from tkinter import *
-from tkinter.filedialog import *
-from tkinter import scrolledtext
+
+
+
 
 
 
@@ -20,22 +20,26 @@ model.load_weights('mnist_weights_epoch10.h5')
 def clicked():
     op = askopenfilename()  
     # Проверка изображения
-    if(len(sys.argv) == 1):
+    if (len(sys.argv) == 1):
         img = cv2.imread(op)
-        if(not img.data):
+        if (not img.data):
             print("Could not load image")
             exit
-            
+
         # предварительная обработка
+        # Меняем цветовое пространство на RGB
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # меняем размер
         img = cv2.resize(img, (28, 28), interpolation = cv2.INTER_AREA)
+        # конвертирование в числа
         img = cv2.bitwise_not(img)
         img = img.reshape(1,28,28,1)
         img = img.astype('float32')
         img /= 255    
-    
+
         # Предсказание рукописной цифры во входном изображении
         score = model.predict(img, batch_size=1, verbose=0)
+
         
         # Показ результатов 
         
@@ -49,7 +53,7 @@ def clicked():
        
 window = Tk()
 window.geometry('470x600')
-window.title("Добро пожаловать в приложение PythonRu")
+window.title("Распознавание рукописных цифр ")
 txt = scrolledtext.ScrolledText(window, width=40, height=35)  
 txt.grid(column=0, row=0) 
 btn = Button(window, text="Открыть", command=clicked,background="#555",foreground="#ccc",padx="20",pady="8",font="16")   
--- mnist_train.py
+++ mnist_train.py
@@ -1,23 +1,31 @@
 from __future__ import print_function
 import keras
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.datasets import mnist  # подпрограммы для извлечения набора данных MNIST
+from keras.models import Sequential  # последовательное описание модели
+from keras.layers import Dense, Dropout, Flatten  # Dense - слой из 10 нейронов.
+# Главная идея Dropout — вместо обучения одной DNN обучить ансамбль нескольких DNN, а затем усреднить полученные результаты.
+# Flatten - слой выравнивания. Он служит соединительным узлом между слоями.
 from keras.layers import Conv2D, MaxPooling2D
+# Первые 2 слоя – Conv2D. Эти сверточные слои будут работать с входными изображениями, которые рассматриваются как двумерные матрицы.
+# MaxPooling2D - слои подвыборки по максимальному значению.
 from keras import backend as K
 import pylab
 import matplotlib.pyplot as plt
 
-batch_size = 128
-num_classes = 10
-epochs = 12
+batch_size = 128  # batch_size — количество обучающих образцов, обрабатываемых одновременно за одну итерацию алгоритма градиентного спуска;
+num_classes = 10  # число выходов
+epochs = 3  # эпохи - количество итераций
 
 # Размер изображения
 img_rows, img_cols = 28, 28
 
-#Данные, разделенные между train и test 
+# Данные, разделенные между train и test
+# Загружаем данные x_train и x_test содержат двухмерный массив с изображение цифр
+# x_test, y_test массив с проверочными данными сети.
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+# Трансформируем из двухмерного массива в трех мерный(28х28х1 канал)
+# Функция reshape() изменяет форму массива без изменения его данных.
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
@@ -27,6 +35,7 @@ else:
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
 
+# Изменение типов данных
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
@@ -39,40 +48,54 @@ print(x_test.shape[0], 'test samples')
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
+# создание модели
 model = Sequential()
+# Добавляем слой
+# Первый слой будет сверточный двухмерный (Conv2D) .
+# Эти сверточные слои будут работать с входными изображениями, которые рассматриваются как двумерные матрицы.
+# kernel_size=3 — размер ядра 3х3.
+# Функция активации 'relu' ( Rectified Linear Activation ) , 64 это число ядер свертки( сколько признаком будем искать)
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
+# Второй сверточный слой
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
+# Создаем вектор для полносвязной сети. Flatten() – слой, преобразующий 2D-данные в 1D-данные.
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu'))  # relu - функция активации для скрытых слоев.
 model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
-#Компиляция модели
+# Создадим однослойный перцептрон
+model.add(Dense(num_classes, activation='softmax'))  #  Функцию активации выходного слоя - softmax, которая применяется для задач классификации.
+# Компиляция модели
+# Оптимизатор весов optimizer='adam' (Адам: метод стохастической оптимизации).
+# Функция потерь : loss='categorical_crossentropy' категориальная перекрестная энтропия (categorical crossentropy CCE).
 model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
+              optimizer=keras.optimizers.Nadam(),
               metrics=['accuracy'])
+# Запуск обучения сети
 # сохраняем историю обучения
+# epochs - число эпох , validation_data=(x_test, y_test) — проверочные данные
 history = model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
 
 # 1 граф
-pylab.figure (1)
-x = range(12)
+pylab.figure(1)
+x = range(3)
 
 plt.grid(True)
 
-plt.plot(x, 
-         history.history['acc'], 
-         'bo-', 
+# График обучения и достоверность значений точности
+plt.plot(x,
+         history.history['acc'],
+         'bo-',
          label='Train accuracy')
 
-plt.plot(x, 
+plt.plot(x,
          history.history['val_acc'],
          'ro-',
          label='Validation accuracy')
@@ -82,24 +105,23 @@ plt.ylabel('Accuracy')
 
 plt.legend(loc='lower right')
 
-
 # вывод итоговых данных
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-# сохранение весов
+# save learned weights
 model.save_weights('mnist_weights_epoch10.h5')
 
 # 2 граф
-pylab.figure (2)
+pylab.figure(2)
 plt.grid(True)
 
-plt.plot(x, 
-         history.history['loss'], 
-         'bo-', 
+plt.plot(x,
+         history.history['loss'],
+         'bo-',
          label='Train losses')
 
-plt.plot(x, 
+plt.plot(x,
          history.history['val_loss'],
          'ro-',
          label='Validation losses')
